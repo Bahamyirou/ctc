ffc00d5fcbfb61988a5872fac9b607f8a607fd47
Bahamyirou <asma.bahamyirou@umontreal.ca>
2025-04-17 14:35:33 -0400

    commit

--- app.R
+++ app.R
@@ -3,13 +3,16 @@ library(shinydashboard)
 library(ggplot2)
 library(readxl)
 library(tidyverse)
+library(shinyWidgets)
 
-load("datactc.RData")
+## Load the data
+load("datactc1.RData")
 
+##
 ui <- dashboardPage(
  
   # A header
-  dashboardHeader(title = "My Dashboard"),
+  dashboardHeader(title = "StatCTC"),
   
   # A sidebar
   dashboardSidebar(
@@ -32,91 +35,112 @@ ui <- dashboardPage(
       
       # First tab or page content
       tabItem(tabName = "dashboard",
+              
               fluidRow(
                 
-                #box1
-                box(plotOutput("plot1", height = 250)),
+                         #box1
+                         box(plotOutput("plot1", height = 250)),
                 
-                #box4
-                box(plotOutput("plot2", height = 250)) ,
+                         #box4
+                         box(plotOutput("plot2", height = 250)) ,
                 
-                #box4
-                box(plotOutput("plot3", height = 250)) ,
+                         #box4
+                          box(plotOutput("plot3", height = 250)) ,
                 
-                #box2
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                ),
+                         #box2
+                         box(
+                              title = "Controls",
+                              sliderInput("slider", "Number of observations:", 1, 100, 50)
+                           ),
                 
-                #box3
-                box(
-                  title = "xxx",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
+                         #box3
+                         box(
+                              title = "xxx",
+                              sliderInput("slider", "Number of observations:", 1, 100, 50)
+                            )
                 
                 
-              ),
+                  ),
               
               # infoBoxes with fill=TRUE
               fluidRow(
-                infoBox("New Orders", 10 * 2, icon = icon("credit-card"), fill = TRUE),
-                infoBoxOutput("progressBox"),
-                infoBoxOutput("approvalBox2")
-              )
-      ),
+                         infoBox("New Orders", 10 * 2, icon = icon("credit-card"), fill = TRUE),
+                        infoBoxOutput("progressBox"),
+                        infoBoxOutput("approvalBox2")
+                      )
+           ),
+      
       
       # Second tab or page content
-      tabItem(
-        
-        tabName = "widgets",
-        
-        h1("Communauté Togolaise Au Canada (CTC)"),
-        
-        
-        h2("La CTC est un organisme à
-                  but non lucratif qui représente les Togolais vivant au Canada. Depuis 32 ans, notre mission est de favoriser la solidarité entre la diaspora togolaise, de promouvoir la culture togolaise et d’intervenir sur les questions cruciales concernant notre pays d’origine, le Togo."),
-        
-        
-        
-        fluidRow(
-          box(
-            title = "NOTRE MISSION", width = 4, background = "yellow",
-            "Servir la communauté togolaise au Canada tout en promouvant l’intégration et l’échange de richesses culturelles."
-          ),
-          box(
-            title = "NOTRE VISION", width = 4, background = "red",
-            "Contribuer au bien-être des personnes originaires du Togo au Canada et à l’essor socio-économique du Canada."
-          ),
-          box(
-            title = "NOS VALEURS",width = 4, background = "green",
-            "Service, Respect et intégrité, Excellence, Équité"
-          )
-        ),
-        
-        
-        # infoBoxes with fill=TRUE
-        fluidRow(
-          #infoBox("New Orders", 10 * 2, icon = icon("credit-card"), fill = TRUE),
-          #infoBoxOutput("progressBox"),
-          #infoBoxOutput("approvalBox2")
-          valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
-          # Dynamic valueBoxes
-          valueBoxOutput("progressBox"),
-          valueBoxOutput("approvalBox")
-        ),
-        p("Le tableau de bord a été mis à jour pour la dernière fois le xxx")
-      ),
       
-      # third tab content
       tabItem(
         
+                   tabName = "widgets",
         
-        tabName = "dashboardsection",
-        h2("À venir ...")
+                   h1("Communauté Togolaise Au Canada (CTC)"),
         
         
-      ),
+                  h2("La CTC est un organisme à  but non lucratif qui représente les Togolais vivant au Canada. Depuis 32 ans, 
+                      notre mission est de favoriser la solidarité entre la diaspora togolaise, de promouvoir la culture togolaise 
+                       et d’intervenir sur les questions cruciales concernant notre pays d’origine, le Togo."),
+                  
+                 h2("Ce tableau de bord fournit des statistiques sur les membres de la CTC, permet de visualiser la tendance 
+                    des adhésions dans le temps et comprendre la composition de notre communauté afin de mieux la servir."),
+        
+      
+        
+                  p(tags$ul(        tags$li(tags$b(t("NOTRE MISSION:")),t("Servir la communauté togolaise au Canada tout en promouvant l’intégration et l’échange de richesses culturelles.")),
+                                   tags$li(tags$b(t("NOTRE VISION:")),t("Contribuer au bien-être des personnes originaires du Togo au Canada et à l’essor socio-économique du Canada.")),
+                                  tags$li(tags$b(t("NOS VALEURS:")),t("Service, Respect et intégrité, Excellence, Équité")),
+                                 style = "font-size:18px;")
+                           ),
+       
+        
+        
+        
+        # fluidRow(
+        #   box(
+        #    title = "NOTRE MISSION", width = 4, background = "yellow",
+        #    "Servir la communauté togolaise au Canada tout en promouvant l’intégration et l’échange de richesses culturelles."
+        # ),
+        # box(
+        #   title = "NOTRE VISION", width = 4, background = "red",
+        #   "Contribuer au bien-être des personnes originaires du Togo au Canada et à l’essor socio-économique du Canada."
+        # ),
+        # box(
+        #   title = "NOS VALEURS",width = 4, background = "green",
+        #   "Service, Respect et intégrité, Excellence, Équité"
+        # )
+        #  ),
+        
+        
+            # infoBoxes with fill=TRUE
+        
+             fluidRow(
+                             #infoBox("New Orders", 10 * 2, icon = icon("credit-card"), fill = TRUE),
+                             #infoBoxOutput("progressBox"),
+                             #infoBoxOutput("approvalBox2")
+                             valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
+                             # Dynamic valueBoxes
+                             valueBoxOutput("progressBox"),
+                             valueBoxOutput("approvalBox")
+                       ),
+        
+                  p("Le tableau de bord a été mis à jour pour la dernière fois le xxx.")
+        
+        
+         ),
+      
+      
+        # third tab content
+        tabItem(
+        
+        
+                  tabName = "dashboardsection",
+                         h2("À venir ...")
+        
+        
+              ),
       
       # third tab content
       tabItem(
@@ -128,24 +152,31 @@ ui <- dashboardPage(
             solidHeader = TRUE,
             width = 3,
             
+            # Filter by Section
+            pickerInput(inputId = "AllSection",
+                        label = t("Selectionner une section"),
+                        choices =  sort(unique(TrendAdhesion$section)),
+                        selected = first(sort(TrendAdhesion$section)),
+                        multiple = FALSE),
+            
             # Date range slider (THE OUTPUT WILL be call dat - this will contain each min/max that user will chose and use below in servser side for filtration)
             dateRangeInput(inputId = "DateRangeSelected",
-                           label = t("Select date range"),
+                           label = t("Selectionner un intervalle de temps"),
                            start = min(datAdhesion$AdhésionDébut, na.rm = TRUE),
                            end =   max(datAdhesion$AdhésionDébut, na.rm = TRUE),
                            min =   min(datAdhesion$AdhésionDébut, na.rm = TRUE),
                            max =   max(datAdhesion$AdhésionDébut, na.rm = TRUE),
-                           separator = t("to"))
+                           separator = t("à"))
             
           )# column
           ,
           box(
-            width = 12,
+            width = 10,
             fluidRow(
               column(
                 width = 12, 
                 plotOutput(outputId = "plot4", 
-                           height = "600px"))
+                           height = "500px"))
             )
           ) # column  box(plotOutput("plot3", height = 150)) ,
         )
@@ -205,13 +236,13 @@ server <- function(input, output) {
   
   # section qui tient compte du Nb membres actifs
              
-             #datAdhesion <- read.csv('datAdhesion.csv');  datAdhesion$AdhésionDébut <- as.Date(datAdhesion$AdhésionDébut)
   
             
   output$plot4 <- renderPlot({
     
                              #Create the data (filter data based on the select date range that user will chose)
-                                datAdhesion_subset <- datAdhesion %>% dplyr::filter(between(AdhésionDébut, 
+                                datAdhesion_subset <- TrendAdhesion %>% dplyr::filter(section %in% input$AllSection,
+                                                                                      between(AdhésionDébut, 
                                                                                             input$DateRangeSelected[1], 
                                                                                              input$DateRangeSelected[2]) )
     
